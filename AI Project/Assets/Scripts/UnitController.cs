dbd6363a99740ba48904f29411196b64dc1b2fc3
stevejma <35577054+stevejma@users.noreply.github.com>
2018-09-17 20:10:22 -0400

    Playground update

--- AI Project/Assets/Scripts/UnitController.cs
+++ AI Project/Assets/Scripts/UnitController.cs
@@ -8,22 +8,30 @@ public class UnitController : MonoBehaviour {
     private Vector3 lookAtTarget;
 
     private Quaternion playerRotate;
-    private float rotateSpeed = 2;
-    private float speed = 5;
 
+    private float rotateSpeed = 1f;
+    private float speed = 5f;
+    private float radiusOfSat = 2f;
+
+    private bool isMoving = false;
+    
     void Start () {
 
-
     }
-	
-	void Update () {
+
+    void Update() {
+
         // On mouse click unit turns and looks at target
         if (Input.GetMouseButton(0)) {
             SetTargetPosition();
         }
 
-        // Unit also moves to target position
-        Move();
+        // Unit also moves to target position when isMoving = true
+
+        if (isMoving) {
+            Move();
+        }
+
     }
 
     void SetTargetPosition() {
@@ -41,14 +49,27 @@ public class UnitController : MonoBehaviour {
                 targetPosition.z - transform.position.z);
             playerRotate = Quaternion.LookRotation(lookAtTarget);
         }
+
+        // Sets isMoving to true
+        isMoving = true;
+
     }
 
     void Move() {
+
+        Vector3 towards = targetPosition - transform.position;
 
         // Adds rotation speed so that unit does not immediately face the mouse click position
         transform.rotation = Quaternion.Slerp(transform.rotation, playerRotate, rotateSpeed * Time.deltaTime);
 
         // Unit moves towards the target position
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        // If unit moves to the target position, stop moving
+        if (towards.magnitude < radiusOfSat) {
+            isMoving = false;
+        }
+
     }
+
 }
