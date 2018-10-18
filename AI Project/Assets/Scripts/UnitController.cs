83ae94ca70bf560de4bfe869b18e2460398ee4f5
brandon-gibson <bgibso19@uncc.edu>
2018-10-18 10:13:53 -0400

    Started working on gun

--- AI Project/Assets/Scripts/UnitController.cs
+++ AI Project/Assets/Scripts/UnitController.cs
@@ -4,72 +4,92 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour {
 
-    private Vector3 targetPosition;
-    private Vector3 lookAtTarget;
+	private Vector3 targetPosition;
+	private Vector3 lookAtTarget;
 
-    private Quaternion playerRotate;
+	private Quaternion playerRotate;
 
-    [SerializeField] private float rotateSpeed = 1f;
-    [SerializeField] private float speed = 5f;
-    private float radiusOfSat = 2f;
+	[SerializeField] private float rotateSpeed = 1f;
+	[SerializeField] private float speed = 5f;
+	private float radiusOfSat = 2f;
 
-    private bool isMoving = false;
-    
-    void Start () {
+	private bool isMoving = false;
 
-    }
+	void Start () {
 
-    void Update() {
+	}
 
-        // On mouse click unit turns and looks at target
-        if (Input.GetMouseButton(1)) {
-            SetTargetPosition();
-        }
+	void Update() {
 
-        // Unit also moves to target position when isMoving = true
+		// On mouse click unit turns and looks at target
+		if (Input.GetMouseButton(1)) {
+			SetTargetPosition();
+		}
 
-        if (isMoving) {
-            Move();
-        }
+		if (Input.GetMouseButton(0)) {
+			ShootGun();
+		}
 
-    }
+		// Unit also moves to target position when isMoving = true
 
-    void SetTargetPosition() {
+		if (isMoving) {
+			Move();
+		}
 
-        // Raycast to mouse click position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+	}
 
-        if (Physics.Raycast(ray, out hit, 1000)) {
-            targetPosition = hit.point;
+	void ShootGun() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 1000)) {
 
-            // Unit orients itself so that it rotates itself along the x and z plane but does not unnaturally look up on the y plane
-            lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
-                transform.position.y,
-                targetPosition.z - transform.position.z);
-            playerRotate = Quaternion.LookRotation(lookAtTarget);
-        }
 
-        // Sets isMoving to true
-        isMoving = true;
+			GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			go.transform.position = this.transform.position;
+			Rigidbody rb = go.AddComponent<Rigidbody>();
+			Vector3 v3T = hit.point;
+			v3T.z = 10.0f;
+			go.transform.LookAt(Camera.main.ScreenToWorldPoint(v3T));
+			rb.AddRelativeForce(Vector3.forward*1000);
+		}
+	}
 
-    }
+	void SetTargetPosition() {
 
-    void Move() {
+		// Raycast to mouse click position
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
 
-        Vector3 towards = targetPosition - transform.position;
+		if (Physics.Raycast(ray, out hit, 1000)) {
+			targetPosition = hit.point;
 
-        // Adds rotation speed so that unit does not immediately face the mouse click position
-        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotate, rotateSpeed * Time.deltaTime);
+			// Unit orients itself so that it rotates itself along the x and z plane but does not unnaturally look up on the y plane
+			lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
+					transform.position.y,
+					targetPosition.z - transform.position.z);
+			playerRotate = Quaternion.LookRotation(lookAtTarget);
+		}
 
-        // Unit moves towards the target position
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+		// Sets isMoving to true
+		isMoving = true;
 
-        // If unit moves to the target position, stop moving
-        if (towards.magnitude < radiusOfSat) {
-            isMoving = false;
-        }
+	}
 
-    }
+	void Move() {
+
+		Vector3 towards = targetPosition - transform.position;
+
+		// Adds rotation speed so that unit does not immediately face the mouse click position
+		transform.rotation = Quaternion.Slerp(transform.rotation, playerRotate, rotateSpeed * Time.deltaTime);
+
+		// Unit moves towards the target position
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+		// If unit moves to the target position, stop moving
+		if (towards.magnitude < radiusOfSat) {
+			isMoving = false;
+		}
+
+	}
 
 }
